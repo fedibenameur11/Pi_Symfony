ed004856632e2f1689811a03d02b3b40d41f7e2b
wassimbhs36 <wassim.belhajsalem@esprit.tn>
2023-03-08 18:53:12 +0100

    final

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -47,7 +47,7 @@ class UsersAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_users_index'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
